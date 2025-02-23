2384eb1033c96836a0a22f27b1a5c5bd3bcdc312
Fahad Khan <149401131+fahadkhan007@users.noreply.github.com>
2025-02-23 18:31:35 +0530

    Add files via upload

---
+++ class1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    enum days{sun,mon,tue,wed,thur,fri,sat};
+    days day1,day2;
+    day1=sun;
+    day2=fri;
+    cout<<day1<<"\t"<<day2;
+    return 0;
+}
